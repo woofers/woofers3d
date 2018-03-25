dfb5fe343a3161b343794ffae2833f28ccfad6d6
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-03-24 22:55:11 -0700

    Change Color of Floor from Constructor

--- core/src/com/jaxson/lib/gdx/box2d/bodies/Floor.java
+++ core/src/com/jaxson/lib/gdx/box2d/bodies/Floor.java
@@ -6,24 +6,33 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.jaxson.lib.gdx.box2d.bodies.types.SpriteBody;
+import com.badlogic.gdx.graphics.Color;
 
 public class Floor extends SpriteBody
 {
-    public static Texture getTex(float width, float height)
+    private static final float WIDTH = 6f;
+    private static final float HEIGHT = 0.5f;
+
+    private static Texture getTex(float width, float height, Color color)
     {
         int pixelWidth = (int) (width * METERS_TO_PIXELS);
         int pixelHeight = (int) (height * METERS_TO_PIXELS);
 
         Pixmap pixmap
                 = new Pixmap(pixelWidth, pixelHeight, Pixmap.Format.RGBA8888);
-        pixmap.setColor(1f, 1f, 1f, 1f);
+        pixmap.setColor(color);
         pixmap.fillRectangle(0, 0, pixelWidth, pixelHeight);
         return new Texture(pixmap);
     }
 
     public Floor(float x, float y)
     {
-        super(getTex(16f, 0.5f), BodyDef.BodyType.StaticBody, 1f);
+        this(x, y, Color.WHITE);
+    }
+
+    public Floor(float x, float y, Color color)
+    {
+        super(getTex(WIDTH, HEIGHT, color), BodyDef.BodyType.StaticBody, 1f);
         moveTo(new Vector2(x, y));
     }
 
